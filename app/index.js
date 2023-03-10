4d5b7bb5ef11fd3dfa620a031ff35f3bb3d849d0
Conrad Kersten <conrad.kersten@loves.com>
2023-03-09 23:17:32 -0600

    who knows

--- app/index.js
+++ app/index.js
@@ -2,3 +2,21 @@
 function greeting() {
     console.log('the greeting function worked');
 }
+function showPanel() {
+    createPanel();
+}
+function createPanel() {
+    const el = document.getElementById("projects");
+    console.log(el);
+    el === null || el === void 0 ? void 0 : el.addEventListener("click", () => {
+        const sliderEl = document.getElementById("slider");
+        console.log(sliderEl);
+        if (sliderEl) {
+            console.log(sliderEl.hidden);
+            sliderEl.hidden = sliderEl.hidden === true ? false : true;
+        }
+    });
+}
+function init() {
+    createPanel();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 function greeting() {
     console.log('the greeting function worked');
 }
+
+function showPanel() {
+    createPanel();
+}
+
+function createPanel() {
+    const el = document.getElementById("projects");
+    console.log(el)
+    el?.addEventListener("click", () => {
+        const sliderEl = document.getElementById("slider");
+        console.log(sliderEl)
+        if (sliderEl) {
+            console.log(sliderEl.hidden);
+            sliderEl.hidden = sliderEl.hidden === true ? false : true;
+        }
+    })
+}
+
+function init() {
+    createPanel();
+}
